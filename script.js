f0c2d64ffd4e9ff48f8c503d9b587c058720e26b
devmichele <166833146+devmichele@users.noreply.github.com>
2024-05-10 14:59:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript para interações e animações
+
+// Exemplo:
+// const banner = document.getElementById("banner");
+// banner.addEventListener("click", function() {
+//     // Ação a ser executada ao clicar no banner
+// });
